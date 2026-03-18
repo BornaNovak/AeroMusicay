d883025480b4e8d7f7ac12db01844c2ae13a1e23
edunovaProgramiranje <84087934+edunovaProgramiranje@users.noreply.github.com>
2026-03-18 19:06:05 +0100

    Ujednačavanje

--- src/pages/izvodaci/IzvodacPregled.jsx
+++ src/pages/izvodaci/IzvodacPregled.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'; // Dodano za praćenje stanja playera
-import { Container, Table, Button, Accordion, Card } from 'react-bootstrap';
-import GlazbaRedak from '../../components/GlazbaRedak';
-import IzvodacServices from '../../services/izvodaci/IzvodacServices';
+import { useEffect, useState } from 'react'; // Dodano za praćenje stanja playera
+import IzvodacService from '../../services/izvodaci/IzvodacService';
 
 export default function IzvodacPregled() {
     // Stanje koje čuva trenutno odabranu pjesmu (u početku je null)
@@ -20,9 +18,10 @@ export default function IzvodacPregled() {
    return(
     <>
     <ol>
-        {izvodaci && izvodaci.map((izvodac)=>{
-            <li>{izvodac.naziv</li>
-        })}
+        {izvodaci && izvodaci.map((izvodac)=>(
+            <li>{izvodac.naziv}</li>
+        ))}
     </ol>
     </>
    )
+}
