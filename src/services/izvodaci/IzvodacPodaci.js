82cbf7642bca77104e05a0ec798f89b54d60ca16
Borna Novak <zamoracborna@gmail.com>
2026-03-19 20:42:20 +0100

    Dodane pjesme

--- src/services/izvodaci/IzvodacPodaci.js
+++ src/services/izvodaci/IzvodacPodaci.js
@@ -39,4 +39,55 @@ export const izvodaci=[
         datumIzdavanja: '1969-12-05T00:00:00',
         akcija:'Nema'
     },
+    {
+        sifra: 4,
+        naziv: 'Creedence Clearwater Revival',
+        zanr: 'Roots Rock, Swamp Rock',
+        pjesma: 'Fortunate Son',
+        album: 'Willy and the Poor Boys',
+        trajanje: 2.21,
+        datumIzdavanja: '1969-11-02T00:00:00',
+        akcija:'Dogovorit cemo se'
+    },
+    {
+        sifra: 5,
+        naziv: 'Cream',
+        zanr: 'Psych Rock, Blues Rock',
+        pjesma: 'Sunshine of Your Love',
+        album: 'Disraeli Gears',
+        trajanje: 4.10,
+        datumIzdavanja: '1967-11-02T00:00:00',
+        akcija:'Ima'
+    },
+    {
+        sifra: 6,
+        naziv: 'Iron Maiden',
+        zanr: 'Heavy Metal',
+        pjesma: 'The Number of the Beast',
+        album: 'The Number of the Beast',
+        trajanje: 4.51,
+        datumIzdavanja: '1982-03-22T00:00:00',
+        akcija:'Ima'
+    },
+    {
+        sifra: 7,
+        naziv: 'Black Sabbath',
+        zanr: 'Heavy Metal, Doom Metal',
+        pjesma: 'Paranoid',
+        album: 'Paranoid',
+        trajanje: 2.48,
+        datumIzdavanja: '1970-09-18T00:00:00',
+        akcija:'ima'
+    },
+    {
+        sifra: 8,
+        naziv: 'The Cult',
+        zanr: 'Hard Rock, Gothic Rock',
+        pjesma: 'She Sells Sanctuary',
+        album: 'Love',
+        trajanje: 4.13,
+        datumIzdavanja: '1985-05-17T00:00:00',
+        akcija:'Nema'
+    },
+    
 ]
