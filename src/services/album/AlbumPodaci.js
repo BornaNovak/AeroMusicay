fbfd493fbbdc2a86c79dbc72b960c8ae5dcc377d
Tomislav Jakopec <tomislav@jakopec.com>
2026-03-30 20:19:44 +0200

    Lagana priprema

---
+++ src/services/album/AlbumPodaci.js
@@ -0,0 +1,17 @@
+export const albumi=[
+    {
+        sifra:1,
+        naziv:'Zeppelin IV',
+        izvodac: 1
+    },
+    {
+        sifra:2,
+        naziv:'Zeppelin II',
+        izvodac: 1
+    },
+    {
+        sifra:3,
+        naziv:'LA Women',
+        izvodac:2
+    }
+]
